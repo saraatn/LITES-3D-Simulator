6c662466748aab032149ffba7bc0c2704682f023
saraatn <saratmw07@gmail.com>
2026-06-08 13:14:01 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,375 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance-1",
+      "name": "entrance 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -2.2137570406980966,
+        "pitch": 0.1306451990390105,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.49042187445575,
+          "pitch": 0.3354220190537198,
+          "rotation": 0,
+          "target": "1-entrance-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.133774271450667,
+          "pitch": 0.3554108895637196,
+          "title": "Station 1",
+          "text": "IoT &amp; Appsheet"
+        }
+      ]
+    },
+    {
+      "id": "1-entrance-3",
+      "name": "entrance 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -2.2607241552712214,
+        "pitch": 0.0749603601043507,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3439802732657267,
+          "pitch": 0.3832021206892904,
+          "rotation": 0,
+          "target": "2-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 1.9017422783282942,
+        "pitch": 0.14823284828743866,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4037000623043774,
+          "pitch": 0.4496381964189773,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8378166187667162,
+          "pitch": 0.2177061992143532,
+          "title": "Station 2",
+          "text": "Pick and pack"
+        }
+      ]
+    },
+    {
+      "id": "3-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -2.2800346682871364,
+        "pitch": 0.036409317764965365,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4276613502486253,
+          "pitch": 0.328562533890949,
+          "rotation": 0,
+          "target": "4-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-8",
+      "name": "8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 2.177614439891336,
+        "pitch": -0.21203073286659446,
+        "fov": 1.4585144351731572
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.367783308670658,
+          "pitch": 0.33482801909883975,
+          "rotation": 0,
+          "target": "5-9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6399415166545968,
+          "pitch": -0.17858421859187779,
+          "title": "Station 3",
+          "text": "Linde"
+        }
+      ]
+    },
+    {
+      "id": "5-9",
+      "name": "9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -2.434543960754997,
+        "pitch": 0.08560047514572133,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2168692067462388,
+          "pitch": 0.3287143918219897,
+          "rotation": 0,
+          "target": "6-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-10",
+      "name": "10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -1.5764005052089054,
+        "pitch": 0.3230314522651039,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1454271890131018,
+          "pitch": 0.5429803133718742,
+          "rotation": 0,
+          "target": "7-14"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3032066327388225,
+          "pitch": -0.17146009576576482,
+          "title": "Station 4",
+          "text": "4 way shutte"
+        },
+        {
+          "yaw": -1.615680121765136,
+          "pitch": 0.13182646913912777,
+          "title": "Station 5",
+          "text": "GoLean"
+        }
+      ]
+    },
+    {
+      "id": "7-14",
+      "name": "14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.4344755797517905,
+        "pitch": 0.1321551195232189,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.45559182252628005,
+          "pitch": 0.3671893352167874,
+          "rotation": 0,
+          "target": "8-15"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-15",
+      "name": "15",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.41887902047865566,
+        "pitch": 0.12764983135765462,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5554075271244141,
+          "pitch": 0.5182057886522049,
+          "rotation": 0,
+          "target": "9-16"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24491000440583477,
+          "pitch": 0.2291153016838976,
+          "title": "Station 6",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Appsheet</span>"
+        }
+      ]
+    },
+    {
+      "id": "9-16",
+      "name": "16",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.5170232731200457,
+        "pitch": 0.14567098401991174,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7064527368620936,
+          "pitch": 0.5567440034636597,
+          "rotation": 0,
+          "target": "10-18"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-18",
+      "name": "18",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 1.9948637309198247,
+        "pitch": 0.160113624607245,
+        "fov": 1.4637180967878773
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
